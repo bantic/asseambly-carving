b84c4fcfe15570858ad991e4fc2bffc3cc4481ec
Cory <cory.forsyth@gmail.com>
2019-07-08 21:45:35 -0400

    little more

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,16 @@
 // The entry file of your WebAssembly module.
 
-export function add(a: i32, b: i32): i32 {
-  return a + b;
+export function writeImageData(size: u32, color: u32): void {
+  for (let i: u32 = 0; i < size; i++) {
+    store<u32>(i << 2, color);
+  }
+}
+
+export function invert(size: u32): void {
+  for (let i: u32 = 0; i < size; i++) {
+    let val = load<u32>(i << 2);
+    let inverted = 0xffffffff - val;
+    inverted = 0xff000000 | inverted; // ensure 0xff full alpha
+    store<u32>(i << 2, inverted);
+  }
 }
